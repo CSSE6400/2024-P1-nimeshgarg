713858b6b221e03cd99a324e3ae13dab6006375b
NIMESH GARG <51132740+nimeshgarg@users.noreply.github.com>
2024-02-22 19:43:54 +1000

    test

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -15,7 +15,7 @@ def get_todos():
         "completed": True,
         "deadline_at": "2023-02-27T00:00:00",
         "created_at": "2023-02-20T00:00:00",
-        "updated_at": "2023-02-20T00:00:00",
+        "updated_at": "2023-02-20T00:00:00"
     })
 
 @api.route('/todos/<int:id>', methods=['GET'])
